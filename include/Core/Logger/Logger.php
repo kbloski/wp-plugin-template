f4a7a93d9b29af3b1ae46ec555f8ba1a6699a081
Kamil Błoński <kblonski02@gmail.com>
2026-01-08 09:51:14 +0100

    update logger

--- include/Core/Logger/Logger.php
+++ include/Core/Logger/Logger.php
@@ -38,27 +38,7 @@ class Logger
 
         // Log do pliku
         error_log($errorMessage, 3, self::get_log_file());
-
-        // Opcjonalnie log do DB (tylko eventy biznesowe)
-        if ($db) {
-            global $wpdb;
-            $table = $wpdb->prefix . 'pluxee_rewards_events';
-            try {
-                $wpdb->insert(
-                    $table,
-                    [
-                        'level' => strtoupper($level),
-                        'message' => $message,
-                        'context' => wp_json_encode($context),
-                        'created_at' => current_time('mysql')
-                    ],
-                    ['%s', '%s', '%s', '%s']
-                );
-            } catch (\Throwable $e) {
-                // fallback do pliku
-                error_log("[LOGGER][DB_FAIL] " . $e->getMessage(), 3, self::get_log_file());
-            }
-        }
+        error_log($errorMessage);
     }
 
     final public static function error(string $message, array $context = [], bool $db = false): void
