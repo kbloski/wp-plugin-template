d53f2f57a8269f6d70514afed94fd9743a7029fb
Kamil Błoński <kblonski02@gmail.com>
2026-01-21 16:31:55 +0100

    Register one file

--- include/Presentation/React/ReactLoader.php
+++ include/Presentation/React/ReactLoader.php
@@ -18,35 +18,62 @@ class ReactLoader extends AbstractSingleton
         $this->scriptHandle = NameBuilder::applyPrefix('assets-react-register_roots');
     }
 
-    public function enqueueScripts()
+    public function enqueueScripts() : void
     {
-        // React roots
-        $js = PluginPaths::getInstance()->getUrl('include/Presentation/React/RegisterRoots.js');
-        $js_path = PluginPaths::getInstance()->getPath('include/Presentation/React/RegisterRoots.js');
+        $script_url = plugins_url('assets/React/index.js', PLUGIN_FILE);
 
-        if (file_exists($js_path)) {
-            wp_enqueue_script(
-                $this->scriptHandle,
-                $js,
-                ['wp-element'],
-                filemtime($js_path),
-                true
-            );
-        }
+        wp_register_script(
+            $this->scriptHandle,
+            $script_url,
+            [], // dependencies jeśli potrzebne np. wp-element
+            filemtime(PluginPaths::getPath('assets/React/index.js')),
+            true
+        );
 
-        
+        wp_enqueue_script($this->scriptHandle);
     }
 
-
-    /**
-     * Dodaje type="module" do naszego handle
-     */
-    public function addTypeModule($tag, $handle, $src)
+    public function addTypeModule($tag, $handle, $src) : string
     {
         if ($handle === $this->scriptHandle) {
-            $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+            $tag = str_replace('<script ', '<script type="module" ', $tag);
         }
-
         return $tag;
+    }
+
+    /**
+     * Generuje inline JS do renderowania React
+     * $componentsMap to tablica w formacie:
+     * [
+     *    'klucz_dom' => 'NazwaKomponentuWModule',
+     *    'hero' => 'HeroComponent',
+     *    'banner' => 'BannerComponent'
+     * ]
+     */
+    public function inlineJs(array $componentsMap) : string 
+    {
+        $mapJson = json_encode($componentsMap);
+        ob_start();
+        ?>
+<script type="module">
+    import * as ComponentsModule from '<?= plugins_url('assets/React/index.js', PLUGIN_FILE) ?>';
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const componentsRegistry = {
+            'hello_react' : ComponentsModule. // 
+        };
+
+        // renderowanie komponentów
+        Object.entries(componentsRegistry).forEach(([key, Component]) => {
+            document.querySelectorAll(`[data-react-root="${key}"]`).forEach(el => {
+                wp.element.createRoot(el).render(
+                    wp.element.createElement(Component, { instance: el.dataset.instance })
+                );
+            });
+        });
+    });
+</script>
+        <?php
+        return ob_get_clean();
     }
 }
