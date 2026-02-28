0586de4bac60b53c134ea549ebd8ba9dcf234802
Kamil Błoński <kblonski02@gmail.com>
2026-02-28 10:04:07 +0100

    .

--- assets/React/Components/Counter.js
+++ assets/React/Components/Counter.js
@@ -2,7 +2,7 @@ const { useState, createElement } = wp.element;
 
 export default function Counter()
 {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(2)
 
     return createElement("div", null, 
         createElement("div", null, `Counterr: (${counter})`),
