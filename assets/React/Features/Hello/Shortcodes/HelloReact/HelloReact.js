c16ca061dea994247247717579591bf903920bea
root <kblonski02@gmail.com>
2026-09-03 13:41:01 +0200

    .

--- assets/React/Features/Hello/Shortcodes/HelloReact/HelloReact.js
+++ assets/React/Features/Hello/Shortcodes/HelloReact/HelloReact.js
@@ -1,28 +1,10 @@
 const ver = window.__plugintemplate.config.version;
-const { createElement, useEffect, lazy} = wp.element;
-const { injectStyleOnce } = await import(`../../../../Utils/injectStyleOnce.js?v=${ver}`);
+const { createElement } = wp.element;
 const { useTranslations } = await import(`../../../../Hooks/useTranslations.js?v=${ver}`)
-
-const styles = `
-    .plugintemplate-hello-react 
-    {
-        border-radius: 4px;
-        box-shadow: 0 0 4px rgba(0,0,0,0.12);
-        text-align: center;
-        background: white;
-        padding: 1rem;
-    }
-`;
 
 export default function HelloReact()
 {
     const t = useTranslations();
-
-    // Styles Loading
-    useEffect(() => {
-        injectStyleOnce("plugintemplate-hello-react", styles)
-    }, []);
-
 
     return createElement(
         'div', { className: 'plugintemplate-hello-react'},
--- assets/React/Utils/injectStyleOnce.js
+++
@@ -1,8 +0,0 @@
-export function injectStyleOnce(id, css = "") {
-    if (!document.getElementById(id)) {
-        const style = document.createElement('style');
-        style.id = id;
-        style.innerHTML = css;
-        document.head.appendChild(style);
-    }
-}
